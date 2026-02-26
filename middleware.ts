19108e41e6edc61b3f44d97217f6fda80e5a6dd9
Rickus Jooste <rickus@MVT.local>
2026-02-26 14:41:54 +0200

    Exclude scout routes from middleware

--- middleware.ts
+++ middleware.ts
@@ -40,6 +40,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|scout|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
