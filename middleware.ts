2de6bca30cd749aee2fdf0beb495459cec5b84ad
Rickus Jooste <rickus@MVT.local>
2026-02-26 15:06:21 +0200

    exclude manifest and icons from middleware

--- middleware.ts
+++ middleware.ts
@@ -40,6 +40,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|scout|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|manifest.json|icon-192.png|icon-512.png|scout|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
